28a7f47b0dbad51772f5b7b15f94de10bb58e39b
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-05-07 13:58:36 +0100

    proptect show book route

--- books/views.py
+++ books/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from .models import Book
 
@@ -6,6 +7,7 @@ def index(req):
     data = Book.objects.all()
     return render(req, 'books/index.html', {'books':data})
 
+@login_required
 def show(req, book_id):
     data = get_object_or_404(Book, pk=book_id)
     print(data)
