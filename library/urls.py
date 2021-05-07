67b0b9fcdd707ddad1c67966d715695e310e9629
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-05-07 14:23:57 +0100

    add default index redirect

--- library/urls.py
+++ library/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, reverse
 
 urlpatterns = [
-    path('', include('books.urls')),
     path('', include('users.urls')),
+    path('books/', include('books.urls')),
     path('admin/', admin.site.urls),
+]
+
+from django.views.generic import RedirectView
+urlpatterns += [
+    path('', RedirectView.as_view(url='books/', permanent=True)),
 ]
