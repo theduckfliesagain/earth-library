677835cc5493b1a4ca62b6d4c3985e7dbc20bea9
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-05-07 10:43:02 +0100

    remove mock data

--- books/views.py
+++ books/views.py
@@ -1,14 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Book
-# Create your views here.
-books_data = [
-    {'id': 1, 'title': 'Life, the Universe and Everything', 'author': 'Douglas Adams'},
-    {'id': 2, 'title': 'The Meaning of Liff', 'author': 'Douglas Adams'},
-    {'id': 3, 'title': 'The No. 1 Ladies\' Detective Agency',
-        'author': 'Alexander McCall Smith'}
-]
-
 
 def index(req):
     data = Book.objects.all()
