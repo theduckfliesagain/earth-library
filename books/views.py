af63b5ae892210fc3b441107a2ac30afef185206
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-05-07 15:44:36 +0100

    update show view with borrowing

--- books/views.py
+++ books/views.py
@@ -1,3 +1,4 @@
+from django.http import request
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
@@ -12,21 +13,38 @@ def index(req):
 
 @login_required
 def show(req, book_id):
-    data = get_object_or_404(Book, pk=book_id)
-    print(data)
-    return render(req, 'books/show.html', {'book': data})
+    book = get_object_or_404(Book, pk=book_id)
+    if req.method == 'POST':
+        form = BorrowBookForm(req.POST)
+
+        if form.is_valid():
+            if(book.borrower == req.user):
+                book.borrower = None
+                msg = f"ACCESS REVOKED: Returned '{book.title}'."
+            else:
+                book.borrower = req.user
+                msg = f"ACCESS ISSUED: Borrowed '{book.title}'."
+
+            book.save()
+            messages.success(req, f"ACCESS ISSUED: Borrowed '{book.title}'.")
+            return redirect('books:show', book_id= book_id)
+    else:
+        form = BorrowBookForm(initial={'borrower': req.user})
+
+    return render(req, 'books/show.html', {'book': book, 'form': form})
 
 
 @login_required
 def new(req):
+    print(req)
     if req.method == 'POST':
         form = NewBookForm(req.POST)
         if form.is_valid():
             new_book_id = form.save().id
             # messages.success(
             #     req, f'ACCESS ISSUED: User {username} granted Level 1 access.')
-            return redirect('books:show', book_id= new_book_id)
+            return redirect('books:show', book_id=new_book_id)
     else:
         form = NewBookForm()
-        
+
     return render(req, 'books/new.html', {'form': form})
