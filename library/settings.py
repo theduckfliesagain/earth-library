749736996e014814f4b83ba04f688bc573e3a8c7
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-05-07 12:14:33 +0100

    simplifiy app refs

--- library/settings.py
+++ library/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'users.apps.UsersConfig',
-    'books.apps.BooksConfig',
+    'users',
+    'books',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
