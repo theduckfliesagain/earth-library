8cfd5a94b44d5072b8ac0728d89db3317696c400
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-05-07 11:01:36 +0100

    init user app and include

--- library/settings.py
+++ library/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'users.apps.UsersConfig'
+    'users.apps.UsersConfig',
     'books.apps.BooksConfig',
     'django.contrib.admin',
     'django.contrib.auth',
